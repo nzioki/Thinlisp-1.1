e9d813c6fd7fd1d1c4b8dd86aa1753d22226fbc3
bhyde <bhyde>
1999-08-01 01:00:04 +0000

    Change copyright on generated GL files.

--- src/tl/c/notify.c
+++ src/tl/c/notify.c
@@ -1,10 +1,24 @@
 /*+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  +
- + Copyright (c) 1995 Gensym Corporation.  All Rights Reserved.
- +
  + Module:      notify.c
  +
- + Author(s):   Jim Allard
+ + Copyright (c) 1999 The Thinlisp Group
+ + Copyright (c) 1995 Gensym Corporation
+ + All Rights Reserved.
+ +
+ + This file is part of ThinLisp.
+ +
+ + ThinLisp is open source; you can redistribute it and/or modify it
+ + under the terms of the ThinLisp License as published by the ThinLisp
+ + Group; either version 1 or (at your option) any later version.
+ +
+ + ThinLisp is distributed in the hope that it will be useful, but
+ + WITHOUT ANY WARRANTY; without even the implied warranty of
+ + MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ +
+ + For additional information see <http://www.thinlisp.org/>
+ +
+ + Author   Jim Allard
  +
  + Description:
  +   Functions for notifying the user, warning the user, informing the user of
